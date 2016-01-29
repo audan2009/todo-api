f3b23d6c5640ca45572df154b39d63fa44001cc0
Dan Nguyen <audan2009@gmail.com>
2016-01-28 21:48:19 -0600

    added query completed

--- server.js
+++ server.js
@@ -15,9 +15,19 @@ app.use(bodyParser.json());
    res.send('Todo API Root')
  });
 
-// GET all /todos
+// GET all /todos  ... /todos?completed=true
 app.get('/todos', function(req, res){
-  res.json(todos);
+  //queryParams returns {completed: 'true'}
+  var queryParams = req.query;
+  var filteredTodos = todos;
+
+  if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true' ) {
+    filteredTodos = _.where(filteredTodos, {completed: true});
+  } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+    filteredTodos = _.where(filteredTodos, {completed: false});
+  }
+
+res.json(filteredTodos);
 });
 
 // GET  /todos/:id before underscore
@@ -25,20 +35,20 @@ app.get('/todos', function(req, res){
 //app.get('/todos/:id', function(req, res){
 //  var todoId = parseInt(req.params.id, 10);
 //  var matchedTodo;
-//  
+//
 //  todos.forEach(function(todo){
 //    if(todo.id === todoId){
 //      matchedTodo = todo;
-//    } 
+//    }
 //  });
 //
 //for loop instead of forEach
 //for(i = 0; i < todos.length; i++){
 //  if (todos[i].id === todoId){
 //    matchedTodo = todos[i];
-//  }   
+//  }
 //};
-//  
+//
 //  if(matchedTodo){
 //      res.json(matchedTodo);
 //  } else {
@@ -62,15 +72,15 @@ app.get('/todos/:id', function(req, res){
 //POST /todos .. add a new todo
 app.post('/todos', function (req, res){
   var body = _.pick(req.body, 'description','completed');
-  
+
   // isBool checking true/false, isString checks if string was provided, trim removes spaces and makes sure no one types a bunch of spaces
   if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
     //400 bad data being passed
     return res.status(400).send();
   } else {
-    
+
     //set body.description to the trimmed value
-    
+
   body.description = body.description.trim();
   body.id = todoNextId++;
 
@@ -82,55 +92,59 @@ app.post('/todos', function (req, res){
 
 //DELETE /todos/:id
 
-app.delete('/todos/:id', function(req, res){
+app.delete('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {id:todoId});
-  
+  var matchedTodo = _.findWhere(todos, {id:todoId})
   if(!matchedTodo){
     res.status(404).send({"error":"no todo found"});
-  } else{  
+  } else {
     //without takes an array, then the full object to delete from matchedTodo
    todos = _.without(todos, matchedTodo);
     //closes request and sends 200 status
     res.json(matchedTodo);
   }
-  
-})
+});
 
-//PUT /todos/:id
+//PUT /todos/:id ...update
 
 app.put('/todos/:id', function(req, res){
+  //this is the body sent in the request, we only want to "pick" these 2 keys
   var body = _.pick(req.body, 'description','completed');
+  //get the id we are looking for from route
   var todoId = parseInt(req.params.id, 10);
+  //find where that id matches in the todo array
   var matchedTodo = _.findWhere(todos, {id:todoId});
   var validAttributes = {};
-  
+
+  //following validation to ensure that whats being updated is valid
+
   if(!matchedTodo){
     return res.status(404).send({"error":"no todo found"});
-  } 
-  
+  }
+
+  //hasOwnProperty checks if the body which is an array has the 'completed' prorperty and is a boolean
   if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
      validAttributes.completed = body.completed;
      } else if(body.hasOwnProperty('completed')){
         return res.status(400).send({"error":"issue with completed"});
-      } 
-  
+      }
+
    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
      validAttributes.description = body.description;
      } else if(body.hasOwnProperty('description')){
         return res.status(400).send({"error":"issue with description"});
-      } 
-  
+      }
+
   //if all IF statements pass then there's something to update
   // you dont have to set matchedTodo = ._extend since objects are passed by reference
-  
+  //_.extend takes an array then updates it with the new validAttributes
+
    _.extend(matchedTodo, validAttributes);
   res.send(matchedTodo);
-  
+
 });
 
 
 app.listen(PORT, function(){
   console.log('Cap, Im listning on ' + PORT + '!');
 });
-
